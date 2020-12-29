67de43304047123f7dea72435b1f3043acbdb7cc
Burak PAMUK <sorswaretablet@gmail.com>
2020-12-29 15:34:16 +0300

    addax tenant service'leri eklendi

---
+++ SwEpApi/Services/Tenants/Addax/Erp/StockPriceListForEpServiceAddax.cs
@@ -0,0 +1,10 @@
+﻿using SwEpApi.Services.Tenants.Base.Erp;
+
+namespace SwEpApi.Services.Tenants.Happyshop
+{
+    public class StockPriceListForEpServiceAddax : StockPriceListForEpServiceBase
+    {
+        public StockPriceListForEpServiceAddax(IConnectionService connectionService) : base(connectionService) { }
+    }
+
+}
