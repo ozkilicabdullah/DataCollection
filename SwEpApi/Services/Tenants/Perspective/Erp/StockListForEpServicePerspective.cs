b9e66deba7cece54ad0ece45702ae9263c6a0ecb
Emine Özgün <sorswaretablet@gmail.com>
2021-02-08 15:08:30 +0300

    Perspective Tenants servisleri eklendi.

---
+++ SwEpApi/Services/Tenants/Perspective/Erp/StockListForEpServicePerspective.cs
@@ -0,0 +1,10 @@
+﻿using SwEpApi.Services.Tenants.Base.Erp;
+
+namespace SwEpApi.Services.Tenants.Perspective
+{
+    public class StockListForEpServicePerspective : StockListForEpServiceBase
+    {
+        public StockListForEpServicePerspective(IConnectionService connectionService) : base(connectionService) { }
+    }
+
+}
---
+++ SwEpApi/Services/Tenants/Perspective/Erp/StockQuantityListForEpServicePerspective.cs
@@ -0,0 +1,10 @@
+﻿using SwEpApi.Services.Tenants.Base.Erp;
+
+namespace SwEpApi.Services.Tenants.Perspective
+{
+    public class StockQuantityListForEpServicePerspective : StockQuantityListForEpServiceBase
+    {
+        public StockQuantityListForEpServicePerspective(IConnectionService connectionService) : base(connectionService) { }
+    }
+
+}
