c36efa721dd177fcf5b6fd3e4eaf0204e63cd326
Emine Özgün <sorswaretablet@gmail.com>
2021-02-09 10:00:10 +0300

    Kip Tenants servisleri eklendi.

---
+++ SwEpApi/Services/Tenants/Kip/Erp/StockListForEpServiceKip.cs
@@ -0,0 +1,10 @@
+﻿using SwEpApi.Services.Tenants.Base.Erp;
+
+namespace SwEpApi.Services.Tenants.Kip
+{
+    public class StockListForEpServiceKip : StockListForEpServiceBase
+    {
+        public StockListForEpServiceKip(IConnectionService connectionService) : base(connectionService) { }
+    }
+
+}
---
+++ SwEpApi/Services/Tenants/Kip/Erp/StockQuantityListForEpServiceKip.cs
@@ -0,0 +1,10 @@
+﻿using SwEpApi.Services.Tenants.Base.Erp;
+
+namespace SwEpApi.Services.Tenants.Kip
+{
+    public class StockQuantityListForEpServiceKip : StockQuantityListForEpServiceBase
+    {
+        public StockQuantityListForEpServiceKip(IConnectionService connectionService) : base(connectionService) { }
+    }
+
+}
