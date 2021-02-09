8386c61585c0237d09ae48c59f924fa9be5d9ec3
Emine Özgün <sorswaretablet@gmail.com>
2021-02-09 18:14:10 +0300

    Ramsey Tenants servisleri eklendi.

---
+++ SwEpApi/Services/Tenants/Ramsey/Erp/StockPriceListForEpServiceKip.cs
@@ -0,0 +1,10 @@
+﻿using SwEpApi.Services.Tenants.Base.Erp;
+
+namespace SwEpApi.Services.Tenants.Ramsey
+{
+    public class StockPriceListForEpServiceRamsey : StockPriceListForEpServiceBase
+    {
+        public StockPriceListForEpServiceRamsey(IConnectionService connectionService) : base(connectionService) { }
+    }
+
+}
