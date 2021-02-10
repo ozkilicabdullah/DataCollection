ddb0bfda2ba11f0846cee55f50eacb16bcc2bda5
Emine Özgün <sorswaretablet@gmail.com>
2021-02-10 15:25:44 +0300

    Bsl için Tenants servisleri eklendi.

---
+++ SwEpApi/Services/Tenants/Bsl/Erp/StockListForEpServiceBsl.cs
@@ -0,0 +1,10 @@
+﻿using SwEpApi.Services.Tenants.Base.Erp;
+
+namespace SwEpApi.Services.Tenants.Bsl
+{
+    public class StockListForEpServiceBsl : StockListForEpServiceBase
+    {
+        public StockListForEpServiceBsl(IConnectionService connectionService) : base(connectionService) { }
+    }
+
+}
