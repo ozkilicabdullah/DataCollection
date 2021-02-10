12d4aa4eab186c938cbb9d3f8b4d52a81a2f899b
Emine Özgün <sorswaretablet@gmail.com>
2021-02-10 13:40:30 +0300

    Oxxo için Tenants servisleri eklendi.

---
+++ SwEpApi/Services/Tenants/Oxxo/Erp/StockListForEpServiceOxxo.cs
@@ -0,0 +1,10 @@
+﻿using SwEpApi.Services.Tenants.Base.Erp;
+
+namespace SwEpApi.Services.Tenants.Oxxo
+{
+    public class StockListForEpServiceOxxo : StockListForEpServiceBase
+    {
+        public StockListForEpServiceOxxo(IConnectionService connectionService) : base(connectionService) { }
+    }
+
+}
