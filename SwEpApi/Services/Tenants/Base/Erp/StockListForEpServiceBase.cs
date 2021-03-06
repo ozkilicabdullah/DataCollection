0cc09078b518ebb8b34aa64fdac31a3d535babd6
Arsene Gen <Ers2107@hotmail.com>
2021-03-06 17:53:30 +0300

    Atrributelar olduğu gibi spden geliyor.

--- SwEpApi/Services/Tenants/Base/Erp/StockListForEpServiceBase.cs
+++ SwEpApi/Services/Tenants/Base/Erp/StockListForEpServiceBase.cs
@@ -20,6 +20,7 @@ namespace SwEpApi.Services.Tenants.Base.Erp
         {
             ConnectionService = connectionService;
         }
+
         public async Task<ResponseModel> Execute(Dictionary<string, object> Payload, string Identifer)
         {
             var response = new ResponseModel
@@ -68,14 +69,30 @@ namespace SwEpApi.Services.Tenants.Base.Erp
                     foreach (EntityStockListForEpBase cust in posts)
                     {
                         List<object> Attributes = new List<object>();
-
-                        Attributes.Add(new { AttributeName = "Sezon", AttributeValue = cust.SeasonName });
-                        Attributes.Add(new { AttributeName = "Renk", AttributeValue = cust.ColorName });
-                        Attributes.Add(new { AttributeName = "Departman", AttributeValue = cust.DepartmentName });
-                        Attributes.Add(new { AttributeName = "Marka", AttributeValue = cust.BrandName });
-                        Attributes.Add(new { AttributeName = "Size", AttributeValue = cust.SizeName });
-
-                        cust.Attributes = Attributes;                        
+                        if (!string.IsNullOrEmpty(cust.AttributesJson))
+                        {
+                            List<Dictionary<string, string>> _attrList = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(cust.AttributesJson);
+                            if(_attrList != null)
+                            {
+                                foreach(Dictionary<string, string> _att1 in _attrList)
+                                {
+                                    foreach (string _attkey in _att1.Keys)
+                                    {
+                                        Attributes.Add(new { AttributeName = _attkey, AttributeValue = _att1[_attkey] });
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Attributes.Add(new { AttributeName = "Sezon", AttributeValue = cust.SeasonName });
+                            Attributes.Add(new { AttributeName = "Renk", AttributeValue = cust.ColorName });
+                            Attributes.Add(new { AttributeName = "Departman", AttributeValue = cust.DepartmentName });
+                            Attributes.Add(new { AttributeName = "Marka", AttributeValue = cust.BrandName });
+                            Attributes.Add(new { AttributeName = "Size", AttributeValue = cust.SizeName });
+                        }
+                        cust.AttributesJson = null;
+                        cust.Attributes = Attributes;
 
                         if (!string.IsNullOrEmpty(cust.ProductImagesJson))
                         {
