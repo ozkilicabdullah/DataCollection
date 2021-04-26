9e5da8feab5dc17709c0d3e4a8dfeffb5c59c464
Emine Özgün <sorswaretablet@gmail.com>
2021-04-26 12:29:49 +0300

    Jimmykey Tenants servisleri eklendi.

---
+++ SwEpApi/Services/Tenants/Jimmykey/Erp/StockPriceListForEpServiceJimmykey.cs
@@ -0,0 +1,10 @@
+﻿using SwEpApi.Services.Tenants.Base.Erp;
+
+namespace SwEpApi.Services.Tenants.Jimmykey
+{
+    public class StockPriceListForEpServiceJimmykey : StockPriceListForEpServiceBase
+    {
+        public StockPriceListForEpServiceJimmykey(IConnectionService connectionService) : base(connectionService) { }
+    }
+
+}
---
+++ SwEpApi/Services/Tenants/Jimmykey/Erp/StockQuantityListForEpServiceJimmykey.cs
@@ -0,0 +1,10 @@
+﻿using SwEpApi.Services.Tenants.Base.Erp;
+
+namespace SwEpApi.Services.Tenants.Jimmykey
+{
+    public class StockQuantityListForEpServiceJimmykey : StockQuantityListForEpServiceBase
+    {
+        public StockQuantityListForEpServiceJimmykey(IConnectionService connectionService) : base(connectionService) { }
+    }
+
+}
