03f5deb22e0eadbb01eb03dea0d33da950c5539e
Abdullah Ozkilic <sorswaretablet@gmail.com>
2021-06-23 09:59:21 +0300

    Addax- InsiderIYS için WebHook eklendi

---
+++ SwEpApi/Services/Tenants/Addax/IYS/IysUpdateUserForEpServiceAddax.cs
@@ -0,0 +1,10 @@
+﻿using SwEpApi.Services.Tenants.Base.IYS;
+using SwEpApi.Services.Tenants.Base.IYS.RequestParams;
+
+namespace SwEpApi.Services.Tenants.Addax
+{
+    public class IysUpdateUserForEpServiceAddax : IysUpdateUserForEpServiceBase
+    {
+        public IysUpdateUserForEpServiceAddax(IConnectionService connectionService) : base(connectionService) { }
+    }
+}
